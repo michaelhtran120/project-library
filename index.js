a2adee995da3ee8ce25a280bc9ae8eab58450753
michaelhtran120 <michaelhtran120@gmail.com>
2021-05-24 15:26:19 -0700

    file updates

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
-let myLibrary = [];
+let myLibrary = [ 
+    {'title': 'The Hobbit',
+    'author':'J.R.R Tolkein',
+    'pages': '100 pgs',
+    'read': true,
+    // 'index': myLibrary.indexOf(this),
+    },
+    {'title': 'HP Sorcerer Stone',
+    'author': 'J.K. Rowling',
+    'pages': '200 pgs',
+    'read': false,
+    // 'index': myLibrary.indexOf(this)
+    }
+];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = `${pages} pages`;
-    this.read = read;
-    // this.info = function() {
-    //     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+
+// Establishing 'Add Book' Button Variable
+let bookBtn = document.getElementById('add-book-btn');
+
+// Event Listener for 'Add Book'
+bookBtn.addEventListener('click', addBook);
+
+let title = document.getElementById('book-title');
+let author = document.getElementById('author-name');
+let pages = document.getElementById('book-pages');
+let read = document.getElementById('read-book');
+
+function addBook(ev) {
+    ev.preventDefault();
+
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = `${pages} pgs`;
+        this.readStatus = read;
+        this.index = myLibrary.length;
+        // this.info = function() {
+        //     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+        // }
+    }
+
+    book = new Book(title.value, author.value, pages.value, read.checked);
+    myLibrary.push(book)
+    document.querySelector('form').reset();
+    displayBook();
+}
+
+function displayBook() {
+    let tableBooks = document.getElementById('table-books');
+    tableBooks.innerHTML = `<tr>
+                                <th>Book Title</th>
+                                <th>Author</th>
+                                <th>Pages</th>
+                                <th>Read?</th>
+                                <th>Delete?<th>
+                            </tr>`
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        let newTableRow = document.createElement('tr');
+        newTableRow.innerHTML = `<td> ${myLibrary[i]['title']} </td>`;
+        newTableRow.innerHTML += `<td> ${myLibrary[i]['author']} </td>`;
+        newTableRow.innerHTML += `<td> ${myLibrary[i]['pages']} </td>`;
+        if (myLibrary[i]['read'] === true) {
+            newTableRow.innerHTML += `<td><input type='checkbox' name='checkbox' value='${i}'checked></td>`;
+        } else {
+            newTableRow.innerHTML += `<td><input type='checkbox' name='checkbox' value='${i}'></td>`
+        }
+        newTableRow.innerHTML += `<td><button class='delete-btn' onclick='deleteBook(${i})'>Delete Book</button></td>`
+        tableBooks.appendChild(newTableRow);
     }
 }
+displayBook();
 
-function addBookToLibrary() {
 
+function deleteBook(id) {
+    myLibrary.splice(id, 1)
+    displayBook();
 }
 
-let title = documentGetElementById('book-title');
-let author = documentGetElementById('author-name');
-let pages = documentGetElementById('book-pages');
-let readBook = documentGetElementById('read-book');
+let checkbox = document.getElementsByName('checkbox');
+
+for (let i = 0; i < checkbox.length; i++) {
+    checkbox[i].addEventListener('change', () =>{
+        if (myLibrary[this.value]['read'] == true) {
+            myLibrary[this.value]['read'] = false;
+        } else {
+            myLibrary[this.value]['read'] = true;
+        }
+    })
+}
